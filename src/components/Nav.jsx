11a9e6de1c32167d9a3d664ec4cf3bae2c86619e
Square <aayushk4980@gmail.com>
2024-05-11 14:40:07 +0530

    Added Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,19 @@ import darkModeEnabledImg from "../images/dark-mode-enabled.svg"
 
 function Nav(props) {
 
+    // If the mode is "light" then set imgURL to darkModeDisabledImg, else set it to darkModeEnabledImg.
+    let imgURL = props.mode === "light" ? darkModeDisabledImg : darkModeEnabledImg;
+
+    // Returning Nav bar.
     return (
         <>
+        <header id="header" className={`flex ${props.mode}-header`}>
+            <h1>Where in the world?</h1>
+            <button id="mode" className="flex" onClick={props.changeTheme}>
+                <img src={imgURL} alt="Change Theme" id="themeImg" />
+                Dark Mode
+            </button>
+        </header>
         </>
     )
 }
