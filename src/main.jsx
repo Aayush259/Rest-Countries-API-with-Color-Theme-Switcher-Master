42fbd4a573737d4d0ab2a5b299e17ccc4bc4630f
Square <aayushk4980@gmail.com>
2024-06-26 23:54:21 +0530

    FIx main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import App from './App.jsx';
 const Home = lazy(() => import('./components/Home.jsx'));
-const FilteredCountry = lazy(() => import('./components/FilteredCountryCards.jsx'));
 const CountryDetail = lazy(() => import('./components/CountryDetail.jsx'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/Where-in-the-world/' element={<App />}>
       <Route path='/Where-in-the-world/' element={<Home />} />
-      <Route path='/Where-in-the-world/filter/all' element={<Home />} />
+      <Route path='/Where-in-the-world/filter/' element={<Home />} />
       <Route path='/Where-in-the-world/filter/:region' element={<Home />} />
       <Route path='/Where-in-the-world/country/:countryName' element={<CountryDetail />} />
     </Route>
