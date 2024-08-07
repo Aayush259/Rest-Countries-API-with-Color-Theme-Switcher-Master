0a11fdd12160ecb69873f5581d021f1c4719a675
Square <aayushk4980@gmail.com>
2024-08-07 13:12:32 +0530

    Added tailwind classes for nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { useCountryDataContext } from '../context/Context.jsx';
 import darkModeDisabledImg from '../images/dark-mode-disabled.svg';
 import darkModeEnabledImg from '../images/dark-mode-enabled.svg';
-import '../styles/header.css';
 
 export default function Nav() {
 
-    const {theme, toggleTheme} = useCountryDataContext();
+    const { theme, toggleTheme } = useCountryDataContext();
 
     // If the theme is "light" then set imgURL to darkModeDisabledImg, else set it to darkModeEnabledImg.
     let imgURL = theme === 'light' ? darkModeDisabledImg : darkModeEnabledImg;
@@ -14,13 +13,28 @@ export default function Nav() {
     // Returning Nav bar.
     return (
         <>
-        <header id='header' className={`flex ${theme}-header`}>
-            <h1 className='bold-800'>Where in the world?</h1>
-            <button id='mode' className='flex bold-600' onClick={toggleTheme}>
-                <img src={imgURL} alt='Change Theme' id='themeImg' />
-                Dark Mode
-            </button>
-        </header>
+            <header
+                className="flex py-4 px-3 sm:px-14 justify-between items-center text-Very-Dark-Blue-Light-Mode-Text bg-white dark:bg-Dark-Blue-Dark-Mode-Elements dark:text-White-Dark-Mode-Text-Light-Mode-Elements shadow-light-box-shadow dark:shadow-dark-box-shadow"
+            >
+
+                <h1
+                    className="text-lg sm:text-2xl font-bold tracking-wide"
+                >
+                    Where in the world?
+                </h1>
+
+                <button
+                    className="flex items-center justify-center gap-1 sm:gap-2 font-semibold text-sm p-2"
+                    onClick={toggleTheme}
+                >
+                    <img
+                        src={imgURL}
+                        alt="Change Theme"
+                        className="h-7 w-3 -rotate-[25deg]"
+                    />
+                    Dark Mode
+                </button>
+            </header>
         </>
     );
 };
